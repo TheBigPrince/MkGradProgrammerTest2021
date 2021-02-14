53b9ad58ab861143c6b8ad1b4a8ccaa9cbc64929
Larry-Dell-Laptop\Laurence Valentini <laurence.val@gmail.com>
2021-02-14 20:01:49 +0800

    implemented ability for user to cycle through selectables (inputs fields, buttons, toggles etc...) by pressing the TAB key when editing clocks

--- Assets/Source/Clocks/Views/EditClockView.cs
+++ Assets/Source/Clocks/Views/EditClockView.cs
@@ -1,8 +1,12 @@
-﻿using Protodroid.Clocks.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Protodroid.Clocks.ViewModels;
 using Protodroid.MVVM;
 using TMPro;
 using UniRx;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Protodroid.Clocks.Views
@@ -15,10 +19,28 @@ namespace Protodroid.Clocks.Views
         [SerializeField]
         private Button saveButton = null, cancelButton = null;
 
+        private List<Selectable> selectables = new List<Selectable>();
+        private int currentlySelectedIndex = 0;
+
+
+        private void Start()
+        {
+            selectables = GetComponentsInChildren<Selectable>().ToList();
+            SetAllSelectableColorsTo(new Color(215,215,215,255));
+        }
+
         protected override void InitialiseBindings()
         {
             base.InitialiseBindings();
-            
+            EventSystem.current.SetSelectedGameObject(null);
+
+            FocusFirstSelectable();
+
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetKeyDown(KeyCode.Tab))
+                .Subscribe(CycleSelectableFocus)
+                .AddTo(Disposer);
+
             titleInput.ObserveEveryValueChanged(field => field.text)
                 .Where(text => text.Length > 0)
                 .Subscribe(title => ViewModel.Title = title)
@@ -41,6 +63,34 @@ namespace Protodroid.Clocks.Views
         protected virtual void ClearInputs(Unit _)
         {
             titleInput.text = "";
+        }
+
+        private void SetAllSelectableColorsTo(Color inColor)
+        {
+            foreach (Selectable selectable in selectables)
+            {
+                ColorBlock colorBlock = selectable.colors;
+                colorBlock.selectedColor = inColor;
+                colorBlock.highlightedColor = inColor;
+                selectable.colors = colorBlock;
+            }
+        }
+        
+        private async void FocusFirstSelectable()
+        {
+            await new WaitForEndOfFrame();
+            currentlySelectedIndex = 0;
+            selectables[currentlySelectedIndex].Select();
+        }
+
+        private void CycleSelectableFocus(long _)
+        {
+            currentlySelectedIndex++;
+            if (currentlySelectedIndex >= selectables.Count) currentlySelectedIndex = 0;
+            
+            if(selectables[currentlySelectedIndex].IsInteractable()) 
+                selectables[currentlySelectedIndex].Select();
+            else CycleSelectableFocus(0);
         }
     }
 }
